20ab4e3e772482d0178da62f2afd398a0b43b82f
Eran Drori <technolp@gmail.com>
2021-02-06 18:21:40 +0200

    Add files via upload

---
+++ CustomProjectSettings.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+
+public class CustomProjectSettings : ScriptableObject
+{
+    public enum Environment { Publish, Sandbox }
+
+    [SerializeField] private Environment environment = Environment.Sandbox;
+
+    [SerializeField] private bool entityLogEnabled;
+
+    [SerializeField] private bool notificationsLogEnabled;
+
+    public static Environment EnvironmentChoosen { get; private set; }
+    public static bool EntityLogEnabled { get; private set; }
+    public static bool NotificationsLogEnabled { get; private set; }
+
+    private void OnEnable()
+    {
+        Refresh(this);
+    }
+
+    public static void Refresh(CustomProjectSettings instance)
+    {
+        EnvironmentChoosen = instance.environment;
+        EntityLogEnabled = instance.entityLogEnabled;
+        NotificationsLogEnabled = instance.notificationsLogEnabled;
+    }
+}
---
+++ CustomProjectSettingsRegister.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+public static class CustomProjectSettingsRegister
+{
+    const string SettingsPath = "Assets/Editor/EranDrori.CustomProjectSettings.asset";
+
+    [SettingsProvider]
+    public static SettingsProvider CreateSettingsProvider()
+    {
+        var logFoldout = true;
+
+        return new SettingsProvider("Project/EranDrori.CustomProjectSettings", SettingsScope.Project)
+        {
+            guiHandler = (searchContext) =>
+            {
+                var settings = Load();
+                var serialized = new SerializedObject(settings);
+
+                EditorGUI.BeginChangeCheck();
+
+                logFoldout = EditorGUILayout.BeginFoldoutHeaderGroup(logFoldout, "Logging");
+
+                if (logFoldout)
+                {
+                    EditorGUILayout.PropertyField(serialized.FindProperty("environment"), new GUIContent("Environment"));
+
+                    EditorGUILayout.PropertyField(serialized.FindProperty("entityLogEnabled"), new GUIContent("Entity"));
+                    EditorGUILayout.PropertyField(serialized.FindProperty("notificationsLogEnabled"), new GUIContent("Notifications"));
+                    EditorGUILayout.HelpBox("Those log messages will appear on console with the prefix [ENTITY] or [NOTIFICATIONS]", MessageType.Info);
+                }
+
+                EditorGUILayout.EndFoldoutHeaderGroup();
+
+                if (EditorGUI.EndChangeCheck())
+                {
+                    serialized.ApplyModifiedProperties();
+                    CustomProjectSettings.Refresh(settings);
+                }
+            },
+
+            keywords = new HashSet<string>(new[] { "Entity log enabled", "Notifications log enabled" })
+        };
+    }
+
+    static CustomProjectSettings Load()
+    {
+        var settings = AssetDatabase.LoadAssetAtPath<CustomProjectSettings>(SettingsPath);
+
+        if (settings == null)
+        {
+            settings = ScriptableObject.CreateInstance<CustomProjectSettings>();
+            AssetDatabase.CreateAsset(settings, SettingsPath);
+            AssetDatabase.SaveAssets();
+        }
+
+        return settings;
+    }
+}
